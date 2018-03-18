15664964139b68125eb532eea524caebb48d495f
toshchev.ivan <toshchev.ivan@gmail.com>
2018-03-18 13:00:19 +0500

    Added eslint configs + stylelint

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' });
 
 const render = pageName => (req, res) => app.render(req, res, `/${pageName}`);
 const handleRequest = (req, res) =>
-    app.getRequestHandler()(req, res, parse(req.url, true));
+  app.getRequestHandler()(req, res, parse(req.url, true));
 
 app.prepare().then(() => {
-    server
-        .get('*', handleRequest)
-        .listen(3000, () => console.log('Listening on http://localhost:3000'));
+  server
+    .get('*', handleRequest)
+    .listen(3000, () => console.log('Listening on http://localhost:3000'));
 });
