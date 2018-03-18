d1971075c312e1c7d1d3045de37ebaaf002663a1
lms <lms@skbkontur.ru>
2018-03-18 14:41:09 +0500

    add hruDb mocks

---
+++ server/repository/hruDb.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.saveMessage = (message) => {
+    
+}
+
+module.exports.saveChat = (chat) => {
+
+}
+
+module.exports.getChat = (chatId) => {
+
+}
+
+module.exports.getUsers = () => {
+    
+}
+
+
+
