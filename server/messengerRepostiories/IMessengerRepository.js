d091ae9b5e52d14152c0b0ca969e5eed64648062
lms <lms@skbkontur.ru>
2018-03-23 10:57:58 +0500

    create interface for IMessageRepositrory

---
+++ server/messengerRepostiories/IMessengerRepository.js
@@ -0,0 +1,47 @@
+
+
+class IMessengerRepositrory {
+
+    saveMessage(message) {
+        //сохраняем 
+    }
+
+    saveUser(user) {
+
+    }
+
+    saveChat(chat) {
+        
+    }
+
+    getUser(userId) {
+
+    }
+
+    getAllMessages(chatId) {
+
+    }
+
+    getMessagesByRange(chatId, oldestMessageId, countMessages) {
+        /*oldestMessageId назввание так себе, смысл что нажно вернуть count сообщений
+         у которых дата создания больше даты startMessage
+        
+         */
+    }
+
+
+    //все аналогично с чатами, кроме выдачи по диапозону, потому что сообщения добавляются снизу, а чаты сверху
+    //только тут не самое старое сообщние, а самый старый чат, который давно не обновлялся
+
+    getAllChats(userId) {
+
+    }
+
+    getChatsByRange(userId, oldestChatId, countChats) {
+        
+    }
+
+    //todo: последние 4 метода можно обобщить по 2, нужно выделить сущьность обобщения
+}
+
+module.exports = IMessengerRepositrory;
