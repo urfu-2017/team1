b32e3df6d9201ab4233831beea58e337c1787303
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2018-03-29 22:23:08 +0500

    Добавила dotenv, поправила немного логику
    refs #15

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const { parse } = require('url');
 
+require('dotenv').config()
 const next = require('next');
 const passport = require('passport');
 const server = require('express')();
 
 const authMiddleware = require('./middleware/auth'); 
 const routes = require('./routes');
-const config = require('../config')
 
 server.use(require('cookie-parser')());
 server.use(require('body-parser').urlencoded({ extended: true }));
-server.use(require('express-session')({ secret: config.secret_key, resave: true, saveUninitialized: true }));;
+server.use(require('express-session')({ secret: process.env.SECRET_KEY, resave: true, saveUninitialized: true }));;
 server.use(passport.initialize());
 server.use(passport.session());
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const config = require('../../config');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 
 passport.use(new GitHubStrategy({
-    clientID: config.GITHUB_CLIENT_ID,
-    clientSecret: config.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${process.env.URL}/auth/github/callback`
   },
   function(accessToken, refreshToken, profile, cb) {
     // Нужна модель пользователя для того чтобы его создавать
