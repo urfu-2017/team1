341fc42aefda364d26c8b533e18326b0d78f238e
lms <lms@skbkontur.ru>
2018-04-09 11:20:15 +0500

    add logging req.user

--- server/middleware/setUp.js
+++ server/middleware/setUp.js
@@ -4,7 +4,17 @@ const dbConnection = require('../db-connection');
 
 
 module.exports = () => async (req, res, next) => {
+    console.log(req.user);
+    if (!req.user) {
+        next();
+        return;
+    }
     const userId = req.user.id;
+    /*{ name: 'Lyapa96',
+    githubId: '11576176',
+    avatar: 'https://avatars1.githubusercontent.com/u/11576176?v=4',
+    chatsIds: [],
+    id: '718c6f82-f46c-4804-80dc-6e29cc5ebe0e' } */
     // нужно уточнить, что собой представляет req.user
     // кажется, это то, что пришло из кук
     // но тогда информации о чатах доверять нельзя
