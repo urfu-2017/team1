adefa772351a72c591fb71e6e5bc27bf621b817c
ToshchevIvan <toshchev.ivan@gmail.com>
2018-04-11 18:36:59 +0500

    Changed userSetup format

--- server/middleware/userSetUp.js
+++ server/middleware/userSetUp.js
@@ -11,25 +11,21 @@ module.exports = () => async (req, res, next) => {
     const userId = req.user.id;
     const chats = await dbConnection.getUserChats(userId);
 
-    const [messages, users] = await Promise.all([
-        Promise.all(chats.map(c => dbConnection.getMessages(c.id))),
-        Promise.all(chats.map(c => dbConnection.getAllChatUsers(c.id)))
-    ]);
-
-    const usersMap = new Map(users.reduce((acc, val) => acc.concat(val), []) // flatten
-        .map(u => [u.id, u]));
+    const messages = await Promise.all(chats.map(c => dbConnection.getMessages(c.id)));
     for (let i = 0; i < chats.length; i += 1) {
         const chat = chats[i];
-        delete chat.usersIds;
-        chat.users = users[i];
-        chat.messages = messages[i]
-            .map(msg => {
-                msg.sender = usersMap.get(msg.senderId);
-                delete msg.senderId;
-                return msg;
-            });
-        chat.lastMessage = chat.messages[chat.messages.length - 1];
+        chat.messages = messages[i];
+        chat.lastMessage = Object.assign({}, chat.messages[chat.messages.length - 1]);
     }
+
+    const lastMessagesUsersIds = chats.map(chat => chat.lastMessage.senderId)
+        .map(id => dbConnection.getUser(id));
+    const lastMessagesUsers = await Promise.all(lastMessagesUsersIds);
+    chats.map((chat, i) => {
+        delete chat.lastMessage.senderId;
+        chat.lastMessage.sender = lastMessagesUsers[i];
+        return chat;
+    });
 
     req.chats = chats;
     next();
