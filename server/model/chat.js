a822229a6bb9e8d1b78068e0cab4ebd051df645b
ToshchevIvan <toshchev.ivan@gmail.com>
2018-04-12 10:46:41 +0500

    Added creatorId to chat

--- server/model/chat.js
+++ server/model/chat.js
@@ -4,18 +4,20 @@ const uuid = require('uuid/v4');
 
 
 class Chat {
-    constructor({ title, picture, usersIds, id }) {
+    constructor({ title, picture, usersIds, id, creatorId }) {
         this.title = title;
         this.picture = picture;
         this.usersIds = usersIds;
         this.id = id;
+        this.creatorId = creatorId;
     }
 
-    static create(title, usersIds, picture = null) {
+    static create(title, usersIds, creatorId, picture = null) {
         return new Chat({
             title,
             picture,
             usersIds,
+            creatorId,
             id: uuid()
         });
     }
